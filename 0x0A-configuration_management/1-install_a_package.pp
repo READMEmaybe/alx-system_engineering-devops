998ec8f5d18b438871aef8083853360bc0b967c7
READMEmaybe <haroune11411@gmail.com>
2023-11-29 01:09:35 +0100

    Fix task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
 # installs flask 2.1.0 using pip3
+package { 'werkzeug':
+  ensure   => '2.0.0',
+  provider => 'pip3',
+}
+
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['werkzeug'],
 }
