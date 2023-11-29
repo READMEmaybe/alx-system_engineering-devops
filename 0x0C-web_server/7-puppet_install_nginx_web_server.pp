4bfe30846931dd075385e81d08a4c6b25d6aac14
READMEmaybe <haroune11411@gmail.com>
2023-11-29 01:38:22 +0100

    Fix test

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -3,14 +3,13 @@ package { 'nginx':
   ensure => 'installed',
 }
 
-
 file { '/var/www/html/index.html':
   ensure  => 'present',
   content => 'Hello World!',
 }
 
 exec { 'append_redirect_me':
-  command => "/usr/bin/sed -i '/^}$/i \ \n\tlocation \/redirect_me {return 301 "Moved Permanently";}' /etc/nginx/sites-available/default",
+  command => "/usr/bin/sed -i '/^}$/i \ \n\tlocation \/redirect_me {return 301 \"Moved Permanently\";}' /etc/nginx/sites-available/default",
 }
 
 service { 'nginx':
