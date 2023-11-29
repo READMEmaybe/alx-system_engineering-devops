cf7df2cd3a3175b1bd97f606ef7dfd99ad2f57e6
READMEmaybe <haroune11411@gmail.com>
2023-11-29 01:22:37 +0100

    Add task 5

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,20 @@
+# puppet manifest to install nginx and redirect /redirect_me
+package { 'nginx':
+  ensure => 'installed',
+}
+
+
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => 'Hello World!',
+}
+
+exec { 'append_redirect_me':
+  command => "/usr/bin/sed -i '/^}$/i \ \n\tlocation \/redirect_me {return 301 "Moved Permanently";}' /etc/nginx/sites-available/default",
+}
+
+service { 'nginx':
+  ensure  => 'running',
+  enable  => true,
+  require => Package['nginx'],
+}
